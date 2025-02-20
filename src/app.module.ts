a12a273e677e5cc592f90caccf7cb7c43656b23c
Sopanha LEAVCHUM <leavchumsopanha@gmail.com>
2025-02-20 14:12:56 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatModule } from './cat/cat.module';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [CatModule, CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
